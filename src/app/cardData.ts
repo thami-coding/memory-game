05a1495c0a429ca8e1372d705cbd288dff97fea7
Thami <sainttsquared@gmail.com>
2025-12-26 03:33:41 +0200

    add card data

---
+++ src/app/cardData.ts
@@ -0,0 +1,103 @@
+import { Apple, Banana, Bird, Bug, Carrot, Fish, Meh, Rabbit, Snail, Turtle } from 'lucide-angular';
+export const cards = [
+  {
+    key: '1',
+    emoji: Rabbit,
+    pairId: 'rabbit',
+  },
+  {
+    key: '2',
+    emoji: Rabbit,
+    pairId: 'rabbit',
+  },
+  {
+    key: '3',
+    emoji: Apple,
+    pairId: 'apple',
+  },
+  {
+    key: '4',
+    emoji: Apple,
+    pairId: 'apple',
+  },
+  {
+    key: '5',
+    emoji: Banana,
+    pairId: 'banana',
+  },
+  {
+    key: '6',
+    emoji: Banana,
+    pairId: 'banana',
+  },
+  {
+    key: '7',
+    emoji: Bird,
+    pairId: 'bird',
+  },
+  {
+    key: '8',
+    emoji: Bird,
+    pairId: 'bird',
+  },
+  {
+    key: '9',
+    emoji: Turtle,
+    pairId: 'turtle',
+  },
+  {
+    key: '10',
+    emoji: Turtle,
+    pairId: 'turtle',
+  },
+  {
+    key: '11',
+    emoji: Snail,
+    pairId: 'snail',
+  },
+  {
+    key: '12',
+    emoji: Snail,
+    pairId: 'snail',
+  },
+  {
+    key: '13',
+    emoji: Meh,
+    pairId: 'meh',
+  },
+  {
+    key: '14',
+    emoji: Meh,
+    pairId: 'meh',
+  },
+  {
+    key: '15',
+    emoji: Carrot,
+    pairId: 'carrot',
+  },
+  {
+    key: '16',
+    emoji: Carrot,
+    pairId: 'carrot',
+  },
+  {
+    key: '17',
+    emoji: Fish,
+    pairId: 'fish',
+  },
+  {
+    key: '18',
+    emoji: Fish,
+    pairId: 'fish',
+  },
+  {
+    key: '19',
+    emoji: Bug,
+    pairId: 'bug',
+  },
+  {
+    key: '20',
+    emoji: Bug,
+    pairId: 'bug',
+  },
+];
